225cce251cf0fe86ad887e44cacd8084bc2a95ab
xxnlxzx <xxnlxzx@gmail.com>
2023-06-17 15:30:08 +0800

    Farhan - Removed 'DTO', Add OneToOne-booking

--- src/main/java/com/sctp/module3project2/services/BerthService.java
+++ src/main/java/com/sctp/module3project2/services/BerthService.java
@@ -1,67 +1,20 @@
 package com.sctp.module3project2.services;
 
-import org.springframework.stereotype.Service;
-
-import com.sctp.module3project2.dto.BerthDto;
 import com.sctp.module3project2.entity.Berth;
-import com.sctp.module3project2.repository.BerthRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class BerthService {
-    private final BerthRepository repository;
+public interface BerthService {
+    List<Berth> getAllBerths();
 
-    public BerthService(BerthRepository repository) {
-        this.repository = repository;
-    }
+    Berth getBerthById(Long id);
 
-    public List<Berth> getAllBerthPortLocations() {
-        return repository.findAll();
-    }
+    Berth createBerth(Berth berth);
 
-    public Berth createBerthPortLocation(BerthDto berthDto) {
-        Berth berth = new Berth();
-        berth.setName(berthDto.getName());
-        berth.setLocation(berthDto.getLocation());
-        berth.setAvailability(true); // Assuming availability is always set to true for new berths.
-        return repository.save(berth);
-    }
+    Berth saveBerth(Berth berth);
 
-    public Berth getBerthPortLocationById(Long id) {
-        return repository.findById(id).orElse(null);
-    }
+    Berth updateBerth(Long id, Berth berth);
 
-    public Berth updateBerthPortLocation(Long id, Berth updatedBerth) {
-        Berth berth = repository.findById(id).orElse(null);
-        if (berth != null) {
-            berth.setName(updatedBerth.getName());
-            return repository.save(berth);
-        }
-        return null;
-    }
-
-    public boolean deleteBerthPortLocation(Long id) {
-        if (repository.existsById(id)) {
-            repository.deleteById(id);
-            return true;
-        }
-        return false;
-    }
-
-    public List<BerthDto> getAllBerthPortLocationsWithAvailability() {
-        List<Berth> berths = repository.findAll();
-        List<BerthDto> berthDtos = new ArrayList<>();
-
-        for (Berth berth : berths) {
-            BerthDto berthDto = new BerthDto();
-            berthDto.setId(berth.getId());
-            berthDto.setName(berth.getName());
-            berthDto.setAvailability(berth.isAvailability());
-            berthDtos.add(berthDto);
-        }
-
-        return berthDtos;
-    }
+    void deleteBerth(Long id);
+    // void deleteBerthPortLocation(Long id);
 }
